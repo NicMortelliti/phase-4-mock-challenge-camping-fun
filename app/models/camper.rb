e98e6f81cdb6e6e1ee3c93ca2baf35a26d82f6a3
NicMortelliti <npmortelliti@gmail.com>
2022-08-09 12:12:37 -0700

    use validates inclusion

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,6 +1,6 @@
 class Camper < ApplicationRecord
   validates :name, presence: true
-  validates :age, numericality: { greater_than_or_equal_to: 8, less_than_or_equal_to: 18 }
+  validates_inclusion_of :age, :in => 8..18
 
   has_many :signups
   has_many :activities, through: :signups
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,5 +1,5 @@
 class Signup < ApplicationRecord
-  validates :time, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23} 
+  validates_inclusion_of :time, :in => 0..23
 
   belongs_to :camper
   belongs_to :activity

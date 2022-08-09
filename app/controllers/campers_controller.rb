aaffddfa8ee3a0a3f430f9e88ef42a7857d439f1
NicMortelliti <npmortelliti@gmail.com>
2022-08-08 17:26:47 -0700

    Add camper not found message

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,4 +1,6 @@
 class CampersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
     campers = Camper.all
     render json: campers, Serializer: CamperSerializer
@@ -7,5 +9,11 @@ class CampersController < ApplicationController
   def show
     camper = Camper.find(params[:id])
     render json: camper, Serializer: CamperShowSerializer
+  end
+
+    private
+
+  def render_not_found_response
+    render json: { error: "Camper not found" }, status: :not_found
   end
 end
